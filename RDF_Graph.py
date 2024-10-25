70ab284b3d2dc08785ff1ee6a2844313bf03908e
FirasCh3 <firaschebil61@gmail.com>
2024-10-25 21:04:48 +0100

    finished building the graph and visualization of the graph

--- RDF_Graph.py
+++ RDF_Graph.py
@@ -1,10 +1,44 @@
 import xml.etree.ElementTree as ET
+import networkx as nx
+import matplotlib.pyplot as plt
+import regex as re
+from sys import argv
 class RDF_Graph:
     def __init__(self, path):
-        tree = ET.parse(path)
-        root = tree.getroot()
-    def traverse_file(self, root):
+        self.tree = ET.parse(path)
+        self.root = self.tree.getroot()
+        self.graph = nx.DiGraph()
+        self.edge_labels = {}
+    def build_graph(self, root):
         if root is not None:
-            print(root.tag)
             for child in root:
-                self.traverse_file(child)
+                root.tag = re.sub(r'\{[^()]*\}','',root.tag)
+                if not root.tag == "RDF":
+                    child.tag = re.sub(r'\{[^()]*\}','', child.tag)
+                    parent = list(root.attrib.values())[0]
+                    if child.text is not None:
+                        self.edge_labels[(parent, child.text)] = child.tag
+                        self.graph.add_edges_from([[parent, child.text]])
+                    else:
+                        self.edge_labels[(parent, list(child.attrib.values())[0])] = child.tag
+                        self.graph.add_edges_from([[parent, list(child.attrib.values())[0]]])
+                self.build_graph(child)
+  
+                       
+    def show_graph(self):
+        self.build_graph(self.root)
+        pos = nx.circular_layout(self.graph)
+        plt.figure(3, figsize=(16,16))
+        nx.draw_networkx(self.graph, pos=pos, node_size=[2000 for i in list(pos.keys())])
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=self.edge_labels)
+        plt.axis("off")
+        plt.savefig("graph.jpeg")
+        plt.show()
+if len(argv)>1:
+    print("argv[0]= ",argv[1])
+    rdf_graph = RDF_Graph(argv[1])
+else:
+    path = input("Path to RDF file: ")
+    rdf_graph = RDF_Graph(path)
+
+rdf_graph.show_graph()
---
+++ README.md
@@ -0,0 +1,6 @@
+A simple visualizer for RDF files
+
+Steps of installation:
+1- git clone https://github.com/FirasCh3/RDF-Graph.git
+2- pip install -r requirements.txt
+3-python -u RDF_Graph.py 'path_to_rdf_file'
